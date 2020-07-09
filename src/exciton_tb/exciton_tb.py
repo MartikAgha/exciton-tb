2d737097a9f23bf95906a9cd981459d3272ead0d
MartikAgha <martikaghajan@gmail.com>
2020-07-09 02:51:24 +0100

    Remove unused functions for now.

---
+++ src/exciton_tb/exciton_tb.py
@@ -0,0 +1,357 @@
+#!/usr/bin/python3
+from itertools import product
+
+import scipy as sp
+import numpy as np
+import h5py as hp
+import numpy.linalg as napla
+
+from .exciton_tools import get_complex_zeros, tr_keld, \
+                           get_cumulative_positions, reduced_tb_vec, \
+                           get_supercell_positions
+
+def cplx_exp_dot(vec1, vec2):
+    return np.exp(-1j*np.dot(vec1, vec2))
+
+class ExcitonTB:
+
+    four_point_str = 'k(%d,%d,%d,%d)'
+    two_point_str = 'k(%d,%d)'
+
+    hermitian_msg = "{} != {} , Not hermitian"
+    hermitian_rounding_dp = 8
+
+    default_args = {'keldysh_r0': 33.875,
+                    'substrate_dielectric': 3.8}
+
+    def __init__(self, hdf5_input, potential='keldysh', args=None):
+        self.potential = potential
+        self.interaction_args = self.default_args if args is None else args
+
+        self.file_storage = hp.File(hdf5_input, 'r')
+        # Split is always True for this calculation
+        self.alat = float(np.array(self.file_storage['crystal']['alat']))
+        self.a1 = np.array(self.file_storage['crystal']['avecs']['a1'])
+        self.a2 = np.array(self.file_storage['crystal']['avecs']['a2'])
+        self.b1 = np.array(self.file_storage['crystal']['gvecs']['b1'])
+        self.b2 = np.array(self.file_storage['crystal']['gvecs']['b2'])
+
+        # FOR NOW: Assume one orbital per atom
+        self.n_atoms = int(np.array(self.file_storage['crystal']['n_atoms']))
+        self.n_orbs = int(np.array(self.file_storage['crystal']['n_orbs']))
+        self.n_k = int(np.array(self.file_storage['eigensystem']['n_k']))
+        self.n_val = int(np.array(self.file_storage['eigensystem']['n_val']))
+        self.n_con = int(np.array(self.file_storage['eigensystem']['n_con']))
+        self.k_grid = np.array(self.file_storage['eigensystem']['k_grid'])
+        self.r_grid = get_supercell_positions(self.a1, self.a2, self.n_k)
+
+        self.motif_vectors = np.array(self.file_storage['crystal']['motif'])
+        self.centre_point = np.array(self.file_storage['crystal']['centre'])
+        orb_pattern = list(self.file_storage['crystal']['orb_pattern'])
+        self.cumulative_positions = get_cumulative_positions(orb_pattern,
+                                                             self.n_orbs)
+
+    def get_eh_int(self, nat, nk, pos_list):
+        b1, b2 = self.b1/nk, self.b2/nk
+        nk2 = nk**2
+
+        if nk == 1:
+            eh_int = list(sp.zeros((nk2, nat, nat), dtype=complex))
+            for m1, m2 in product(range(nk), range(nk)):
+                pos = pos_list[m1*nk + m2]
+                # We must recentre all into the first unit cell, which we
+                #can then displace by the lattice vector
+                mat_term = np.array([
+                    [self.fourier_keld(pos, i, j, None) for j in range(nat)]
+                    for i in range(nat)
+                ], dtype=complex)
+
+                eh_int[m1*nk + m2] += mat_term
+        else:
+            # This only need be done if we actually sample k-points
+            eh_int = list(sp.zeros((4*nk2, nat, nat), dtype=complex))
+
+            for ml1, ml2 in product(range(2*nk), range(2*nk)):
+                kdiff = (ml1 - nk)*b1 + (ml2 - nk)*b2
+                for r1, r2 in product(range(nk), range(nk)):
+                    pos = pos_list[r1*nk + r2]
+                    mat_term = np.array([
+                        [self.fourier_keld(pos, i, j, kdiff)/nk2
+                         for j in range(nat)]
+                        for i in range(nat)
+                    ], dtype=complex)
+
+                    eh_int[ml1*2*nk + ml2] += mat_term
+        return eh_int
+
+    def get_vector_diff_modulo_cell(self, i, j):
+        """
+        Get separation between two points in the unit cell, modulo'd by
+        lattice vectors so that the separation vector is within the unit cell.
+        """
+        tdiff = self.motif_vectors[i] - self.motif_vectors[j]
+        n1 = (np.dot(tdiff, self.b1)/2/np.pi)//1
+        n2 = (np.dot(tdiff, self.b2)/2/np.pi)//1
+        tij = tdiff - n1*self.a1 - n2*self.a2
+        return tij
+
+    def fourier_keld(self, pos, i, j, kpt):
+        radius_0 = self.interaction_args['keldysh_r0']
+        alat = self.alat
+        ep12 = 0.5*(1 + self.interaction_args['substrate_dielectric'])
+
+        tij = self.get_vector_diff_modulo_cell(i, j)
+        tij_cent = tij - self.centre_point
+        r_vec = pos + tij_cent
+
+        fourier_term = tr_keld(napla.norm(r_vec), radius_0, ep12, alat)
+        if kpt is not None:
+            fourier_term *= cplx_exp_dot(kpt, r_vec)
+
+        return fourier_term
+
+    def get_matrix_dimensions(self, split=True):
+        """
+        Get matrix dimensions.
+        :param split:
+        :return:
+        """
+        n_val, n_con, nk2 = self.n_con, self.n_val, self.n_k**2
+        mat_dim = [n_val*n_con*nk2, n_val*n_con*nk2] if split else 2*n_val*n_con*nk2
+        final_cumul, rdim, rdim_spl = None, 0, [0, 0]
+        # Make cumulative positions of elements in matrix to navigate
+        cmax_s, vmin_s = [], []
+
+        two_point_str = 'k(%d,%d)'
+        if bool(np.array(self.file_storage['selective'])):
+            cumul = []
+            cumul_spl = [[], []]
+            for m1, m2 in product(range(self.n_k), range(self.n_k)):
+                vmin = list(self.file_storage[two_point_str % (m1, m2)]['Vmin'])
+                cmax = list(self.file_storage[two_point_str % (m1, m2)]['Cmax'])
+
+                for s0 in range(2):
+                    if split:
+                        cumul_spl[s0].append(rdim_spl[s0])
+                        rdim_spl[s0] += (n_val - vmin[s0])*(cmax[s0] - n_val)
+                        cmax_s.append(cmax[s0])
+                        vmin_s.append(vmin[s0])
+                    else:
+                        cumul.append(rdim)
+                        rdim += (n_val - vmin[s0])*(cmax[s0] - n_val)
+            mat_dim = rdim_spl if split else rdim
+            final_cumul = cumul_spl if split else cumul
+
+        return mat_dim, final_cumul, cmax_s, vmin_s
+
+    def get_conduction_indices(self, m1, m2, l1, l2, s0, f):
+        if bool(np.array(f['selective'])):
+            c_num_m = list(f[self.two_point_str % (m1, m2)]['Cmax'])[s0] - self.n_val
+            c_num_l = list(f[self.two_point_str % (l1, l2)]['Cmax'])[s0] - self.n_val
+        else:
+            c_num_m, c_num_l = self.n_con, self.n_con
+
+        return c_num_m, c_num_l
+
+    def get_valence_indices(self, m1, m2, l1, l2, s0, f):
+
+        if bool(np.array(f['selective'])):
+            v_min_m = list(f[self.two_point_str % (m1, m2)]['Vmin'])[s0]
+            v_min_l = list(f[self.two_point_str % (l1, l2)]['Vmin'])[s0]
+            v_num_m = self.n_val - list(f[self.two_point_str % (m1, m2)]['Vmin'])[s0]
+            v_num_l = self.n_val - list(f[self.two_point_str % (l1, l2)]['Vmin'])[s0]
+        else:
+            v_min_m, v_min_l = 0, 0
+            v_num_m, v_num_l = self.n_val, self.n_val
+
+        return v_min_m, v_min_l, v_num_m, v_num_l
+
+    def create_matrix_element_hdf5(self, element_storage):
+        """
+        Create matrix elements for the direct coulomb interaction
+        :param element_storage: name for the hdf5 storage for matrix elements.
+        :return:
+        """
+        self.element_storage_name = element_storage
+        """Create store of eigenvalues, matrix elements and attributes."""
+        # Acquire atomic structure parameters.
+        nat = self.n_atoms
+        norb = self.n_orbs
+        nk, nk2 = self.n_k, self.n_k**2
+
+        # Containers for outputted data
+        pos_list = self.r_grid
+
+        with hp.File(element_storage, 'w') as f:
+            eigvecs = np.array(self.file_storage['eigensystem']['eigenvectors'])
+            eh_int = self.get_eh_int(nat, nk, pos_list)
+            f['VkM'] = eh_int
+            f['selective'] = False  #for now
+            nk_shift = nk if nk == 1 else 2*nk
+
+            for s0 in range(2):
+                # Two separate sets for up and down spin for non-exchange
+                # Assuming a Gamma-point calculation for now in supercell
+                spins = f.create_group('s{}'.format(s0))
+
+                for m1, m2 in product(range(nk), range(nk)):
+                    for l1, l2 in product(range(nk), range(nk)):
+                        # Indices to navigate through k,k' data
+                        if nk > 1:
+                            ml1, ml2 = m1 - l1 + nk, m2 - l2 + nk
+
+                        else:
+                            ml1, ml2 = (m1 - l1) % nk, (m2 - l2) % nk
+
+                        ki_m, ki_l = m1*nk + m2, l1*nk + l2
+                        m_shf, l_shf = ki_m*norb, ki_l*norb
+                        id_string = self.four_point_str % (m1, m2, l1, l2)
+                        kpts = spins.create_group(id_string)
+
+                        valence_idx = self.get_valence_indices(
+                            m1, m2, l1, l2, s0, f
+                        )
+                        v_min_m, v_min_l, v_num_m, v_num_l = valence_idx
+
+                        conduction_idx = self.get_conduction_indices(
+                            m1, m2, l1, l2, s0, f
+                        )
+                        c_num_m, c_num_l = conduction_idx
+
+                        e_m, e_l = m_shf + s0*norb, l_shf + s0*norb
+
+                        valence = np.zeros(
+                            (nat, v_num_m*v_num_l),
+                            dtype='complex'
+                        )
+
+                        vb_iter = product(range(v_num_m), range(v_num_l))
+                        for v1, v2 in vb_iter:
+                            # for v1, k' and v2, k find matrix element
+                            # This is reversed compared to conduction band
+                            vm_i = v2*v_num_m + v1
+                            valence[:, vm_i] = reduced_tb_vec(
+                                eigvecs[e_l:e_l + norb, v2 + v_min_l],
+                                eigvecs[e_m:e_m + norb, v1 + v_min_m],
+                                nat,
+                                self.cumulative_positions
+                            )
+                        conduction = np.zeros(
+                            (nat, c_num_m*c_num_l),
+                            dtype='complex'
+                        )
+                        cb_iter = product(range(c_num_m), range(c_num_l))
+                        for c1, c2 in cb_iter:
+                            # for c1, k and c2, k' find matrix element
+                            cm_i = c1*c_num_l + c2
+                            conduction[:, cm_i] = reduced_tb_vec(
+                                eigvecs[e_m:e_m + norb, self.n_val + c1],
+                                eigvecs[e_l:e_l + norb, self.n_val + c2],
+                                nat,
+                                self.cumulative_positions
+                            )
+                        elem_shape = (c_num_m*c_num_l, v_num_m*v_num_l)
+                        mmm = kpts.create_dataset(name='mat_elems',
+                                                  shape=elem_shape,
+                                                  dtype='complex')
+
+                        mmm[:] = np.dot(np.array(conduction).T,
+                                        np.dot(eh_int[ml1*nk_shift + ml2],
+                                               np.array(valence)))
+
+                        del conduction, valence
+
+    def get_diag_eigvals(self, k_idx, kq_idx, q_crys, direct, h5_file):
+
+        energy_k = np.array(h5_file['eigensystem']['eigenvalues'][k_idx])
+
+        if direct or (q_crys[0] == 0 and q_crys[1] == 0):
+            # This doesn't create another instance
+            energy_kq = energy_k
+        else:
+            energy_kq = np.array(h5_file['eigensystem']['eigenvalues'][kq_idx])
+
+        return energy_k, energy_kq
+
+    def read_eigenvectors(self, idx_pair):
+        i1, i2 = idx_pair[0], idx_pair[1]
+        eigvecs = np.array(
+            self.file_storage['eigensystem']['eigenvalues'][i1:i2, :]
+        )
+        return eigvecs
+
+    def read_scatter_matrix(self, element_storage, s_str, k_str):
+        return np.array(element_storage[s_str][k_str]['mat_elems'])
+
+    def get_bse_eigensystem_direct(self, matrix_element_storage):
+        f = self.file_storage
+        g = hp.File(matrix_element_storage, 'r')
+        nk, nk2 = self.n_k, self.n_k**2
+        norb, nat = self.n_orbs, self.n_atoms
+        n_val, n_con = self.n_val, self.n_con
+
+        mat_dim, cumulSpl, Cmax_s, Vmin_s = self.get_matrix_dimensions(
+            split=True
+        )
+        block_skip = n_con*n_val
+        spin_shift = n_con + n_val
+        bse_mat = [get_complex_zeros(mat_dim[i]) for i in range(2)]
+        q_zero = np.array([0, 0])
+
+        for m1, m2 in product(range(nk), range(nk)):
+            k_i = nk*m1 + m2
+
+            # Diagonal elements
+            energy_k, energy_kq = self.get_diag_eigvals(k_idx=k_i,
+                                                        kq_idx=k_i,
+                                                        q_crys=q_zero,
+                                                        direct=True,
+                                                        h5_file=f)
+
+            for s0 in range(2):
+                k_skip = k_i*(block_skip//2)
+                # Note: Should the below line use kp_i?
+                cnum1, vnum1 = n_con, n_val
+
+                for c, v in product(range(cnum1), range(vnum1)):
+                    mat_idx = k_skip + c*vnum1 + v
+
+                    final_energy = energy_kq[n_val + c + s0*spin_shift]
+                    init_energy = energy_k[s0*spin_shift + (n_val - vnum1) + v]
+                    energy_diff = final_energy - init_energy
+                    bse_mat[s0][mat_idx, mat_idx] = energy_diff
+            # No longer needed in memory,
+            del energy_k, energy_kq
+
+            ######################### OFF-DIAGONAL ELEMENTS ##################
+            for l1, l2 in product(range(nk), range(nk)):
+                kp_i = nk*l1 + l2
+                kkp_1bz = [m1, m2, l1, l2]
+
+                for s0 in range(2):
+                    scatter_int = self.read_scatter_matrix(
+                        element_storage=g,
+                        s_str='s%d' % s0,
+                        k_str=self.four_point_str % tuple(kkp_1bz)
+                    )
+
+                    k_skip = k_i*(block_skip//2)
+                    kp_skip = kp_i*(block_skip//2)
+                    cnum1, cnum2 = n_con, n_con
+                    vnum1, vnum2 = n_val, n_val
+
+                    cb_iter = product(range(cnum1), range(cnum2))
+                    for c1, c2 in cb_iter:
+                        vb_iter = product(range(vnum1), range(vnum2))
+
+                        for v1, v2 in vb_iter:
+                            fin_idx = k_skip + c1*vnum1 + v1
+                            init_idx = kp_skip + c2*vnum2 + v2
+
+                            sc_idx = c1*cnum2 + c2, v2*vnum1 + v1
+                            int_term = scatter_int[sc_idx[0], sc_idx[1]]
+
+                            bse_mat[s0][fin_idx, init_idx] -= int_term
+
+        exciton_eigensystems = [napla.eigh(bse_mat[i]) for i in range(2)]
+        return exciton_eigensystems
---
+++ src/exciton_tb/exciton_tools.py
@@ -0,0 +1,105 @@
+from subprocess import Popen
+from itertools import product
+
+import numpy as np
+import scipy as sp
+import scipy.special as spec
+
+def remove(file):
+    """Remove file."""
+    Popen("rm -rf %s" % file, shell=True)
+
+def get_complex_zeros(square_dimension):
+    return sp.zeros((square_dimension, square_dimension), dtype=complex)
+
+def cplx_exp_dot(vec1, vec2):
+    return np.exp(-1j*np.dot(vec1, vec2))
+
+def hermite_mat_prod(mat_1, mat_2):
+    return sp.mat(np.dot(np.conj(mat_1.T), mat_2))
+
+def tr_keld(radius, radius_0, ep12, alat):
+    """Truncated Keldysh potential."""
+
+    main_radius = alat if radius < 1e-5 else radius
+
+    term_arg = (main_radius / radius_0)*ep12
+    struve_term = spec.struve(0, term_arg)
+    bessel_term = spec.y0(term_arg)
+
+    truncated_keldysh = 22.59/radius_0*(struve_term - bessel_term)
+
+    return truncated_keldysh
+
+def supercell_term(ukc1, ukpc2, ukv1, ukpv2, vkm, nc):
+    """BSE matrix element for supercell."""
+    matrix_element_vec_left = reduced_tb_vec(ukc1, ukpc2, nc)
+    matrix_element_vec_right = np.dot(vkm, reduced_tb_vec(ukpv2, ukv1, nc))
+
+    return np.dot(matrix_element_vec_left, matrix_element_vec_right)
+
+def recentre(m1, m2, nk):
+    """Given corner-centred coords, finds midpoint-centred coords."""
+    return [m1 - int(m1 > nk // 2)*nk, m2 - int(m2 > nk // 2)*nk]
+
+def recentre_idx(radius, i, j, nc, a1, a2):
+    """Given corner-cetred indices, create spatial vector."""
+    rec_idx = recentre((i % nc - j % nc) % nc, (i//nc - j//nc) % nc, nc)
+    return radius + rec_idx[0]*a1 + rec_idx[1]*a2
+
+def conj_dot(vec1, vec2):
+    """Returns complex vector inner product <vec1|vec2>"""
+    return np.dot(np.conj(vec1.T), vec2)
+
+def get_supercell_positions(a1, a2, nk):
+    position_list = []
+    for m1, m2 in product(range(nk), range(nk)):
+        ms = recentre(m1, m2, nk)
+        r_position = ms[0]*a1 + ms[1]*a2
+        position_list.append(r_position)
+    return position_list
+
+
+def get_cumulative_positions(pattern, norb):
+
+    p_sum = sum(pattern)
+    p_len = len(pattern)
+    if norb % p_len != 0:
+        raise Exception("Pattern does not fit periodically in orbital set")
+    cumul_term = lambda i: (i//p_len)*p_sum + sum(pattern[:(i % p_len) + 1])
+    cumulative_positions = [0] + [cumul_term(i) for i in range(norb)]
+    return cumulative_positions
+
+
+def reduced_tb_vec(v1, v2, nat, cumul_pos):
+    """
+    Reduces two TB vectors to a partial-inner-product vector, by collapsing
+    only the orbital space.
+    :param v1: first vector
+    :param v2: second vector
+    :param nat: number of atoms in the system
+    :param cumul_pos: Cumulative position of each atom in the index list of
+                      orbitals.
+    :return: reduced vector in the subspace of atoms alone (not orbitals)
+    """
+    reduced_vec = np.array([
+        conj_dot(v1[cumul_pos[i]:cumul_pos[i + 1]],
+                 v2[cumul_pos[i]:cumul_pos[i + 1]])
+        for i in range(nat)
+    ])
+
+    return reduced_vec
+
+def get_band_extrema(eigensystem, energy_limit, cell_size):
+
+    max_energy_1 = eigensystem[0][0][cell_size ** 2 - 1] + energy_limit
+    max_energy_2 = eigensystem[1][0][cell_size ** 2 - 1] + energy_limit
+    min_energy_1 = eigensystem[0][0][cell_size ** 2] - energy_limit
+    min_energy_2 = eigensystem[1][0][cell_size ** 2] - energy_limit
+
+    cb_max_1 = list(eigensystem[0][0] < max_energy_1).index(False) + 1
+    cb_max_2 = list(eigensystem[1][0] < max_energy_2).index(False) + 1
+    vb_min_1 = list(eigensystem[0][0] >= min_energy_1).index(True)
+    vb_min_2 = list(eigensystem[1][0] >= min_energy_2).index(True)
+
+    return cb_max_1, cb_max_2, vb_min_1, vb_min_2
