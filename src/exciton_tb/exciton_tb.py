0cb17d3260a3d56f67ae46ffa0026b1c7053214c
MartikAgha <martikaghajan@gmail.com>
2020-07-22 11:08:35 +0100

    Rearrange functions so the user-interactive methods are at the top for easy reference.

--- src/exciton_tb/exciton_tb.py
+++ src/exciton_tb/exciton_tb.py
@@ -69,6 +69,186 @@ class ExcitonTB:
                 self.file_storage['band_edges']['num_states']
             ))
 
+    def create_matrix_element_hdf5(self, element_storage):
+        """
+        Create matrix elements for the direct coulomb interaction
+        :param element_storage: name for the hdf5 storage for matrix elements.
+        :return:
+        """
+        self.element_storage_name = element_storage
+        """Create store of eigenvalues, matrix elements and attributes."""
+        # Acquire atomic structure parameters.
+        nat = self.n_atoms
+        norb = self.n_orbs
+        nk, nk2 = self.n_k, self.n_k**2
+
+        # Containers for outputted data
+        pos_list = self.r_grid
+
+        with hp.File(element_storage, 'w') as f:
+            eigvecs = np.array(self.file_storage['eigensystem']['eigenvectors'])
+            eh_int = self.get_eh_int(nat, nk, pos_list)
+            f['VkM'] = eh_int
+            nk_shift = nk if nk == 1 else 2*nk
+
+            num_states = self.num_states if self.selective_mode else norb
+
+            for s0 in range(2):
+                # Two separate sets for up and down spin for non-exchange
+                # Assuming a Gamma-point calculation for now in supercell
+                spins = f.create_group('s{}'.format(s0))
+
+                for m1, m2 in product(range(nk), range(nk)):
+                    for l1, l2 in product(range(nk), range(nk)):
+                        # Indices to navigate through k,k' data
+                        if nk > 1:
+                            ml1, ml2 = m1 - l1 + nk, m2 - l2 + nk
+
+                        else:
+                            ml1, ml2 = (m1 - l1) % nk, (m2 - l2) % nk
+
+                        ki_m, ki_l = m1*nk + m2, l1*nk + l2
+
+                        bandnum_m = self.get_number_conduction_valence_bands(
+                            ki_m, s0
+                        )
+                        v_num_m, c_num_m = bandnum_m
+
+                        bandnum_l = self.get_number_conduction_valence_bands(
+                            ki_l, s0
+                        )
+                        v_num_l, c_num_l = bandnum_l
+
+                        valence = np.zeros((nat, v_num_m*v_num_l),
+                                           dtype='complex')
+                        conduction = np.zeros((nat, c_num_m*c_num_l),
+                                              dtype='complex')
+
+                        m_shf, l_shf = ki_m*2*num_states, ki_l*2*num_states
+                        id_string = self.four_point_str % (m1, m2, l1, l2)
+                        kpts = spins.create_group(id_string)
+                        e_m, e_l = m_shf + s0*num_states, l_shf + s0*num_states
+
+                        vb_iter = product(range(v_num_m), range(v_num_l))
+                        for v1, v2 in vb_iter:
+                            # for v1, k' and v2, k find matrix element
+                            # This is reversed compared to conduction band
+                            vm_i = v2*v_num_m + v1
+                            valence[:, vm_i] = reduced_tb_vec(
+                                eigvecs[e_l:e_l + num_states, v2],
+                                eigvecs[e_m:e_m + num_states, v1],
+                                nat,
+                                self.cumulative_positions
+                            )
+                        cb_iter = product(range(c_num_m), range(c_num_l))
+                        for c1, c2 in cb_iter:
+                            # for c1, k and c2, k' find matrix element
+                            cm_i = c1*c_num_l + c2
+                            conduction[:, cm_i] = reduced_tb_vec(
+                                eigvecs[e_m:e_m + norb, v_num_m + c1],
+                                eigvecs[e_l:e_l + norb, v_num_l + c2],
+                                nat,
+                                self.cumulative_positions
+                            )
+                        elem_shape = (c_num_m*c_num_l, v_num_m*v_num_l)
+                        mmm = kpts.create_dataset(name='mat_elems',
+                                                  shape=elem_shape,
+                                                  dtype='complex')
+
+                        mmm[:] = np.dot(np.array(conduction).T,
+                                        np.dot(eh_int[ml1*nk_shift + ml2],
+                                               np.array(valence)))
+
+                        del conduction, valence
+
+    def get_bse_eigensystem_direct(self, matrix_element_storage, solve=True):
+        """
+        Construct the Hamiltonian (and solve it if necessary) for the
+        Bethe-Salpeter Equation for the direct photonic transitions (Q=0).
+        :param matrix_element_storage: HDF5 Storage for the matrix elements.
+        :param solve: Set to True to find the eigenvalues, otherwise will
+                      output the matrix.
+        :return:
+        """
+        g = hp.File(matrix_element_storage, 'r')
+        nk, nk2 = self.n_k, self.n_k**2
+        n_val, n_con = self.n_val, self.n_con
+        selective = self.selective_mode
+
+        mat_dim, blocks = self.get_matrix_dim_and_block_starts(split=True)
+        block_skip = n_con*n_val
+        spin_shift = n_con + n_val
+        bse_mat = [get_complex_zeros(mat_dim[i]) for i in range(2)]
+        q_zero = np.array([0, 0])
+
+        for m1, m2 in product(range(nk), range(nk)):
+            k_i = nk*m1 + m2
+
+
+            # Diagonal elements: Use cb/vb energy differences
+            energy_k, energy_kq = self.get_diag_eigvals(k_idx=k_i,
+                                                        kq_idx=k_i,
+                                                        q_crys=q_zero,
+                                                        direct=True)
+
+            for s0 in range(2):
+                k_skip = blocks[s0][k_i] if selective else k_i*block_skip
+                s_skip = self.num_states if selective else spin_shift
+                vnum1, cnum1 = self.get_number_conduction_valence_bands(
+                    k_i, s0
+                )
+
+                for c, v in product(range(cnum1), range(vnum1)):
+                    mat_idx = k_skip + c*vnum1 + v
+                    final_energy = energy_kq[vnum1 + c + s0*s_skip]
+                    init_energy = energy_k[v + s0*s_skip]
+                    energy_diff = final_energy - init_energy
+                    bse_mat[s0][mat_idx, mat_idx] = energy_diff
+
+            # No longer needed
+            del energy_k, energy_kq
+
+            # Off-diagonal elements:
+            for l1, l2 in product(range(nk), range(nk)):
+                kp_i = nk*l1 + l2
+                kkp_1bz = [m1, m2, l1, l2]
+                for s0 in range(2):
+                    # Read out precalculated scattering matrix
+                    k_str = self.four_point_str % tuple(kkp_1bz)
+                    s_str = self.spin_str % s0
+                    scatter_int = np.array(g[s_str][k_str]['mat_elems'])
+
+                    k_skip = blocks[s0][k_i] if selective else k_i*block_skip
+                    kp_skip = blocks[s0][kp_i] if selective else kp_i*block_skip
+
+                    vnum1, cnum1 = self.get_number_conduction_valence_bands(
+                        k_i, s0
+                    )
+                    vnum2, cnum2 = self.get_number_conduction_valence_bands(
+                        kp_i, s0
+                    )
+
+                    for c1, c2 in product(range(cnum1), range(cnum2)):
+                        for v1, v2 in product(range(vnum1), range(vnum2)):
+                            fin_idx = k_skip + c1*vnum1 + v1
+                            init_idx = kp_skip + c2*vnum2 + v2
+
+                            sc_idx = c1*cnum2 + c2, v2*vnum1 + v1
+                            int_term = scatter_int[sc_idx[0], sc_idx[1]]
+
+                            # Note the sign of the direct integral
+                            bse_mat[s0][fin_idx, init_idx] -= int_term
+
+        g.close()
+        # Decide whether to solve or output the hamiltonian to be
+        # diagonalised elsewhere
+        if solve:
+            exciton_eigensystems = [napla.eigh(bse_mat[i]) for i in range(2)]
+        else:
+            exciton_eigensystems = bse_mat
+
+        return exciton_eigensystems
+
     def terminate_storage_usage(self):
         """Terminate the current usage session of the HDF5 file."""
         self.file_storage.close()
@@ -203,98 +383,6 @@ class ExcitonTB:
 
         return vb_num, cb_num
 
-    def create_matrix_element_hdf5(self, element_storage):
-        """
-        Create matrix elements for the direct coulomb interaction
-        :param element_storage: name for the hdf5 storage for matrix elements.
-        :return:
-        """
-        self.element_storage_name = element_storage
-        """Create store of eigenvalues, matrix elements and attributes."""
-        # Acquire atomic structure parameters.
-        nat = self.n_atoms
-        norb = self.n_orbs
-        nk, nk2 = self.n_k, self.n_k**2
-
-        # Containers for outputted data
-        pos_list = self.r_grid
-
-        with hp.File(element_storage, 'w') as f:
-            eigvecs = np.array(self.file_storage['eigensystem']['eigenvectors'])
-            eh_int = self.get_eh_int(nat, nk, pos_list)
-            f['VkM'] = eh_int
-            nk_shift = nk if nk == 1 else 2*nk
-
-            num_states = self.num_states if self.selective_mode else norb
-
-            for s0 in range(2):
-                # Two separate sets for up and down spin for non-exchange
-                # Assuming a Gamma-point calculation for now in supercell
-                spins = f.create_group('s{}'.format(s0))
-
-                for m1, m2 in product(range(nk), range(nk)):
-                    for l1, l2 in product(range(nk), range(nk)):
-                        # Indices to navigate through k,k' data
-                        if nk > 1:
-                            ml1, ml2 = m1 - l1 + nk, m2 - l2 + nk
-
-                        else:
-                            ml1, ml2 = (m1 - l1) % nk, (m2 - l2) % nk
-
-                        ki_m, ki_l = m1*nk + m2, l1*nk + l2
-
-                        bandnum_m = self.get_number_conduction_valence_bands(
-                            ki_m, s0
-                        )
-                        v_num_m, c_num_m = bandnum_m
-
-                        bandnum_l = self.get_number_conduction_valence_bands(
-                            ki_l, s0
-                        )
-                        v_num_l, c_num_l = bandnum_l
-
-                        valence = np.zeros((nat, v_num_m*v_num_l),
-                                           dtype='complex')
-                        conduction = np.zeros((nat, c_num_m*c_num_l),
-                                              dtype='complex')
-
-                        m_shf, l_shf = ki_m*2*num_states, ki_l*2*num_states
-                        id_string = self.four_point_str % (m1, m2, l1, l2)
-                        kpts = spins.create_group(id_string)
-                        e_m, e_l = m_shf + s0*num_states, l_shf + s0*num_states
-
-                        vb_iter = product(range(v_num_m), range(v_num_l))
-                        for v1, v2 in vb_iter:
-                            # for v1, k' and v2, k find matrix element
-                            # This is reversed compared to conduction band
-                            vm_i = v2*v_num_m + v1
-                            valence[:, vm_i] = reduced_tb_vec(
-                                eigvecs[e_l:e_l + num_states, v2],
-                                eigvecs[e_m:e_m + num_states, v1],
-                                nat,
-                                self.cumulative_positions
-                            )
-                        cb_iter = product(range(c_num_m), range(c_num_l))
-                        for c1, c2 in cb_iter:
-                            # for c1, k and c2, k' find matrix element
-                            cm_i = c1*c_num_l + c2
-                            conduction[:, cm_i] = reduced_tb_vec(
-                                eigvecs[e_m:e_m + norb, v_num_m + c1],
-                                eigvecs[e_l:e_l + norb, v_num_l + c2],
-                                nat,
-                                self.cumulative_positions
-                            )
-                        elem_shape = (c_num_m*c_num_l, v_num_m*v_num_l)
-                        mmm = kpts.create_dataset(name='mat_elems',
-                                                  shape=elem_shape,
-                                                  dtype='complex')
-
-                        mmm[:] = np.dot(np.array(conduction).T,
-                                        np.dot(eh_int[ml1*nk_shift + ml2],
-                                               np.array(valence)))
-
-                        del conduction, valence
-
     def get_diag_eigvals(self, k_idx, kq_idx, q_crys, direct):
         """
         Get the diagonal elements of the bse matrix,
@@ -326,91 +414,3 @@ class ExcitonTB:
             self.file_storage['eigensystem']['eigenvalues'][i1:i2, :]
         )
         return eigvecs
-
-    def get_bse_eigensystem_direct(self, matrix_element_storage, solve=True):
-        """
-        Construct the Hamiltonian (and solve it if necessary) for the
-        Bethe-Salpeter Equation for the direct photonic transitions (Q=0).
-        :param matrix_element_storage: HDF5 Storage for the matrix elements.
-        :param solve: Set to True to find the eigenvalues, otherwise will
-                      output the matrix.
-        :return:
-        """
-        g = hp.File(matrix_element_storage, 'r')
-        nk, nk2 = self.n_k, self.n_k**2
-        n_val, n_con = self.n_val, self.n_con
-        selective = self.selective_mode
-
-        mat_dim, blocks = self.get_matrix_dim_and_block_starts(split=True)
-        block_skip = n_con*n_val
-        spin_shift = n_con + n_val
-        bse_mat = [get_complex_zeros(mat_dim[i]) for i in range(2)]
-        q_zero = np.array([0, 0])
-
-        for m1, m2 in product(range(nk), range(nk)):
-            k_i = nk*m1 + m2
-
-
-            # Diagonal elements: Use cb/vb energy differences
-            energy_k, energy_kq = self.get_diag_eigvals(k_idx=k_i,
-                                                        kq_idx=k_i,
-                                                        q_crys=q_zero,
-                                                        direct=True)
-
-            for s0 in range(2):
-                k_skip = blocks[s0][k_i] if selective else k_i*block_skip
-                s_skip = self.num_states if selective else spin_shift
-                vnum1, cnum1 = self.get_number_conduction_valence_bands(
-                    k_i, s0
-                )
-
-                for c, v in product(range(cnum1), range(vnum1)):
-                    mat_idx = k_skip + c*vnum1 + v
-                    final_energy = energy_kq[vnum1 + c + s0*s_skip]
-                    init_energy = energy_k[v + s0*s_skip]
-                    energy_diff = final_energy - init_energy
-                    bse_mat[s0][mat_idx, mat_idx] = energy_diff
-
-            # No longer needed
-            del energy_k, energy_kq
-
-            # Off-diagonal elements:
-            for l1, l2 in product(range(nk), range(nk)):
-                kp_i = nk*l1 + l2
-                kkp_1bz = [m1, m2, l1, l2]
-                for s0 in range(2):
-                    # Read out precalculated scattering matrix
-                    k_str = self.four_point_str % tuple(kkp_1bz)
-                    s_str = self.spin_str % s0
-                    scatter_int = np.array(g[s_str][k_str]['mat_elems'])
-
-                    k_skip = blocks[s0][k_i] if selective else k_i*block_skip
-                    kp_skip = blocks[s0][kp_i] if selective else kp_i*block_skip
-
-                    vnum1, cnum1 = self.get_number_conduction_valence_bands(
-                        k_i, s0
-                    )
-                    vnum2, cnum2 = self.get_number_conduction_valence_bands(
-                        kp_i, s0
-                    )
-
-                    for c1, c2 in product(range(cnum1), range(cnum2)):
-                        for v1, v2 in product(range(vnum1), range(vnum2)):
-                            fin_idx = k_skip + c1*vnum1 + v1
-                            init_idx = kp_skip + c2*vnum2 + v2
-
-                            sc_idx = c1*cnum2 + c2, v2*vnum1 + v1
-                            int_term = scatter_int[sc_idx[0], sc_idx[1]]
-
-                            # Note the sign of the direct integral
-                            bse_mat[s0][fin_idx, init_idx] -= int_term
-
-        g.close()
-        # Decide whether to solve or output the hamiltonian to be
-        # diagonalised elsewhere
-        if solve:
-            exciton_eigensystems = [napla.eigh(bse_mat[i]) for i in range(2)]
-        else:
-            exciton_eigensystems = bse_mat
-
-        return exciton_eigensystems
