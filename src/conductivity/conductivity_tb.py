22e8be72521a27295fd01a54fdc6e9834ae1e7b3
MartikAgha <martikaghajan@gmail.com>
2020-07-25 17:28:03 +0100

    Add function to Conductivity object to calculate the non interacting optical conductivity.

--- src/conductivity/conductivity_tb.py
+++ src/conductivity/conductivity_tb.py
@@ -1,6 +1,11 @@
+from itertools import product
 
 import numpy as np
-import h5py as hp
+
+from .conductivity_tools import velocity_matrix_element, \
+                                get_broadening_function
+
+e_charge_2_over_epsilon0 = 180.79096
 
 
 class ConductivityTB:
@@ -10,36 +15,17 @@ class ConductivityTB:
                             'lh': np.array([1j, 1]),
                             'rh': np.array([-1j, 1])}
 
-    def __init__(self, hdf5_input):
+    def __init__(self, exciton_obj):
         """
-
-        :param hdf5_input:
+        Optical conductivity calculator for tight-binding models
+        :param hdf5_input: Input from the tight-binding model in hdf5 format.
         """
-        self.data_storage = hp.File(hdf5_input, 'r')
-
-        # Remains open for the calculation, use self.terminate_storage_usage
-        # when done using this.
-        # Extract geometric information
-        self.alat = float(np.array(self.data_storage['crystal']['alat']))
-        self.a1 = np.array(self.data_storage['crystal']['avecs']['a1'])
-        self.a2 = np.array(self.data_storage['crystal']['avecs']['a2'])
-        self.b1 = np.array(self.data_storage['crystal']['gvecs']['b1'])
-        self.b2 = np.array(self.data_storage['crystal']['gvecs']['b2'])
-        self.motif_vectors = np.array(self.data_storage['crystal']['motif'])
-
-        self.n_atoms = int(np.array(self.data_storage['crystal']['n_atoms']))
-        self.n_orbs = int(np.array(self.data_storage['crystal']['n_orbs']))
-        self.n_k = int(np.array(self.data_storage['eigensystem']['n_k']))
-        self.n_val = int(np.array(self.data_storage['eigensystem']['n_val']))
-        self.n_con = int(np.array(self.data_storage['eigensystem']['n_con']))
-        self.n_spins = int(np.array(self.data_storage['eigensystem']['n_spins']))
-
-        # Acrue real-space and reciprocal-space grid.
-        self.k_grid = np.array(self.data_storage['eigensystem']['k_grid'])
-
-    def terminate_hdf5_session(self):
-        """Close HDF5 storage used for this class."""
-        self.data_storage.close()
+        self.exciton_obj = exciton_obj
+        self.file_storage = exciton_obj.file_storage
+        self.k_grid = exciton_obj.k_grid
+        self.a1, self.a2 = exciton_obj.a1, exciton_obj.a2
+        self.n_spins = exciton_obj.n_spins
+        self.n_orbs = exciton_obj.n_orbs
 
     def load_velocity_matrix(self, k_idx, s0):
         """
@@ -49,9 +35,9 @@ class ConductivityTB:
         :return: velocity_matrix
         """
         if self.n_spins == 1:
-            vel_mat = np.array(self.data_storage['velocity_matrix'][k_idx])
+            vel_mat = np.array(self.file_storage['velocity_matrix'][k_idx])
         else:
-            vel_mat = np.array(self.data_storage['velocity_matrix'][k_idx][s0])
+            vel_mat = np.array(self.file_storage['velocity_matrix'][k_idx][s0])
         return vel_mat
 
     def convert_str_to_polarisation(self, string):
@@ -66,13 +52,70 @@ class ConductivityTB:
             polarisation_vector = self.polarisation_vectors['x']
         return polarisation_vector
 
-    def serial_conductivity(self,
-                            sigma=0.04,
-                            direction='x',
-                            freq_range=(0.0, 6.0, 1000),
-                            interaction=True):
-        if interaction:
-            pass
-        else:
+    def non_interacting_conductivity(self,
+                                     sigma=0.04,
+                                     freq_range=(0.0, 6.0, 1000),
+                                     dielectric_function=False,
+                                     broadening='lorentz'):
+        """
+        Calculate the optical conductivity in the absence of electron-hole
+        interactions for a range of frequencies.
+        :param sigma: Phenomological broadening given to the conductivity.
+        :param freq_range: tuple (freq_start, freq_finish, freq_number) as
+                           used for numpy.linspace()
+        :param dielectric_function: Set to True to calculate the imaginary
+                                    part of the dielectric function.
+        :param broadening: type of broadening (e.g. 'lorentz', 'gauss')
+        :return:
+        """
+        system_area = np.abs(np.cross(self.a1, self.a2)[2])*len(self.k_grid)
+        broad_fnc = get_broadening_function(broadening, sigma)
+
+        frequency_grid = np.linspace(*freq_range)
+        output_grid = np.zeros(len(frequency_grid))
+
+        energy_power = 1 + int(dielectric_function)
+        prefactor = e_charge_2_over_epsilon0 if dielectric_function else 1
+        prefactor = prefactor/system_area
+
+        n_shift = self.n_orbs if self.n_spins == 1 else 2*self.n_orbs
+        for idx_1, kpt in enumerate(self.k_grid):
+            for s0 in range(2):
+                if self.n_spins == 1 and s0 == 1:
+                    continue
+                velocity_matrix = self.load_velocity_matrix(idx_1, s0)
+                bands = self.exciton_obj.get_number_conduction_valence_bands(
+                    idx_1, s0
+                )
+                v_num, c_num = bands
+                eigvals = np.array(
+                    self.file_storage['eigensystem']['eigenvalues'][idx_1]
+                )
+
+                j1, j2 = n_shift*idx_1, n_shift*(idx_1 + 1)
+                eigvecs = np.array(
+                    self.file_storage['eigensystem']['eigenvectors'][j1:j2, :]
+                )
+                for v, c in product(range(v_num), range(c_num)):
+                    cb_vector = eigvecs[:, v_num + c]
+                    vb_vector = eigvecs[:, v_num]
+                    vb_energy, cb_energy = eigvals[v_num + c], eigvals[v_num]
+                    matrix_elem = velocity_matrix_element(vb_vector,
+                                                          cb_vector,
+                                                          velocity_matrix)
+
+                    for idx_2, omega in enumerate(frequency_grid):
+                        energy_diff = (cb_energy - vb_energy)**energy_power
+                        smearing = broad_fnc(energy_diff, omega)
+                        output = prefactor*matrix_elem/energy_diff*smearing
+                        output_grid[idx_2] = output
+
+        return output_grid
+
+
+
+
+
+
 
 
--- src/conductivity/conductivity_tools.py
+++ src/conductivity/conductivity_tools.py
@@ -1,4 +1,16 @@
+from functools import partial
+
 import numpy as np
+
+
+def get_broadening_function(fnc_name, sigma):
+	if fnc_name is 'lorentz':
+		fnc = partial(lorentz, sigma=sigma)
+	elif fnc_name is 'gauss':
+		fnc = partial(gauss, sigma=sigma)
+	else:
+		raise Exception('Function name not available')
+	return fnc
 
 def gauss(w, w0, sigma):
 	value = np.exp(-((w-w0)/sigma)**2/2)/np.sqrt(2*np.pi)/sigma
@@ -7,3 +19,8 @@ def gauss(w, w0, sigma):
 def lorentz(w, w0, sigma):
 	value = sigma/2/((w-w0)**2 + (sigma/2)**2)/np.pi
 	return value
+
+def velocity_matrix_element(vb_vector, cb_vector, velocity_matrix):
+	matrix_element_vb = np.dot(velocity_matrix, vb_vector)
+	matrix_element = np.abs(np.dot(np.conj(cb_vector), matrix_element_vb))**2
+	return matrix_element
