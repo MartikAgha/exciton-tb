304c7db614ad9649357b1b79f60eac9dc9a1ba49
MartikAgha <martikaghajan@gmail.com>
2020-08-28 13:16:04 +0100

    002-build-testing-suite Create skeleton scripts for the testing of the ExcitonTB object and the exciton tools, and set up the collection of tests to be performed on the module.

---
+++ src/tests/test_exciton_tb.py
@@ -0,0 +1,30 @@
+import unittest
+
+from h5py import File
+
+from exciton_tb.exciton_tb import ExcitonTB
+
+
+class TestExcitonTB(unittest.TestCase):
+
+    def setUp(self):
+        raise NotImplementedError()
+
+    def test_exciton_tb_init(self):
+        raise NotImplementedError()
+
+    def test_hdf5_storage_creation(self):
+        raise NotImplementedError()
+
+    def test_vector_modulo_cell(self):
+        raise NotImplementedError()
+
+    def test_fourier_matrix_creation(self):
+        raise NotImplementedError()
+
+    def test_electron_hole_interaction(self):
+        raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    unittest.main()
